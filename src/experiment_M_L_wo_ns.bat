5e52d550922f6bd91bb6155e9115d731329dbcbc
Yang Liu <thomasliuy@gmail.com>
2020-01-27 10:27:32 -0500

    experiment to test M and L model without unlabeled data

---
+++ src/experiment_M_L_wo_ns.bat
@@ -0,0 +1,1 @@
+python -m training.experiment_M_L_wo_ns -p ..\data\fromraw_cid_inchi_smiles_fp_labels.csv -e 100 -b 256 -l 0.001
---
+++ src/training/experiment_M_L_wo_ns.py
@@ -0,0 +1,75 @@
+""" Experiment to test HLMC_M and HLMC_L model without using Noisy Student 
+labeled unlabeled data.
+"""
+import os
+import argparse
+from datetime import datetime
+
+import tensorflow as tf
+import numpy as np
+
+from models.hmlc import HMLC_M, HMLC_L
+from data_loaders.cvs_loader import CVSLoader
+from utils.label_convertors import convert2vec, hierarchical, convert2hier
+from .train_model import train_model
+
+
+def experiment(data_path,
+               DataLoader=CVSLoader,
+               columns=["ECFP", "Label"],
+               learning_rate=0.001,
+               drop_rate=0.3,
+               batch_size=128,
+               epochs=30,
+               log_path="../logs"):
+    # Data
+    data_loader = DataLoader(data_path)
+    x_train, y_train, x_test, y_test = data_loader.load_data(columns)
+
+    x_train = convert2vec(x_train, dtype=float)
+    y_train = convert2hier(y_train, dtype=float)
+    
+    x_test = convert2vec(x_test, dtype=float)
+    y_val = convert2hier(y_test, dtype=float) # for validation during training
+    y_eval = convert2vec(y_test, dtype=int)   # for evaluation after training
+    
+    data_pred = data_loader.load_unlabeled(["ECFP", "Label"])
+    x_pred = data_pred[:, 0]
+    x_pred = convert2vec(x_pred, dtype=float)
+
+    # Open log
+    now = datetime.now()
+    timestamp = now.strftime(r"%Y%m%d_%H%M%S")
+    log_path = os.path.sep.join(log_path.split("/"))
+    log_path = os.path.join(log_path, timestamp)
+    os.makedirs(log_path, exist_ok=True)
+    log_f_path = os.path.join(log_path, "logs.txt")
+    log_f = open(log_f_path, "w")
+
+    # Train model1
+    ## Initialize model1
+    model1 = HMLC_M(drop_rate=drop_rate)
+    ## Training
+    train_model(
+        model1, x_train, y_train, x_test, y_val, y_eval,
+        learning_rate, drop_rate, batch_size, epochs, log_path, log_f
+    )
+
+    # Train model2
+    ## Initialize model2
+    model1 = HMLC_L(drop_rate=drop_rate)
+    ## Training
+    train_model(
+        model2, x_train, y_train, x_test, y_val, y_eval,
+        learning_rate, drop_rate, batch_size, epochs, log_path, log_f
+    )
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--data-path")
+    parser.add_argument("-b", "--batch-size", type=int)
+    parser.add_argument("-l", "--learning-rate", type=float)
+    parser.add_argument("-e", "--epochs", type=int)
+    args = parser.parse_args()
+    experiment(**vars(args))

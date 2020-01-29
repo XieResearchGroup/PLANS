dfffebd9168657e474805af5198197448a9fdd7a
Yang Liu <thomasliuy@gmail.com>
2020-01-29 14:54:07 -0500

    reformat

--- src/train_hmlc.bat
+++ src/train_hmlc.bat
@@ -1,1 +1,8 @@
-python -m training.train_hmlc -p ..\data\fromraw_cid_inchi_smiles_fp_labels.csv -e 1 -b 256 -l 0.001
+python -m training.train_hmlc ^
+-p ..\data\fromraw_cid_inchi_smiles_fp_labels.csv ^
+-e 1 ^
+-b 256 ^
+-l 0.001 ^
+--es-patience 5 ^
+-m "train hlmc with weights" ^
+--unlabeled-weight 0.5

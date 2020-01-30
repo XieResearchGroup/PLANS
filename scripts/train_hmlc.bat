0e084f7c5c90085b04ec3bb39a6d7ae5df88835a
Yang Liu <thomasliuy@gmail.com>
2020-01-30 16:33:36 -0500

    fix

--- scripts/train_hmlc.bat
+++ scripts/train_hmlc.bat
@@ -1,8 +1,7 @@
-python -m training.train_hmlc ^
--p ..\data\fromraw_cid_inchi_smiles_fp_labels.csv ^
+python -m src.training.train_hmlc ^
+-p .\data\fromraw_cid_inchi_smiles_fp_labels.csv ^
 -e 1 ^
 -b 256 ^
 -l 0.001 ^
 --es-patience 5 ^
--m "train hlmc with weights" ^
 --unlabeled-weight 0.5

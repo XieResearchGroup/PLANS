d2e61f2a52fc660e87130d055244acfb25107506
Yang Liu <thomasliuy@gmail.com>
2020-01-30 11:45:06 -0500

    linting

--- src/training/experiment_hard_label.py
+++ src/training/experiment_hard_label.py
@@ -1,12 +1,11 @@
 import os
-import argparse
 from datetime import datetime
 from functools import partial
 
 import tensorflow as tf
 import numpy as np
 
-from models.hmlc import HMLC, HMLC_M, HMLC_L
+from models.hmlc import HMLC, HMLC_M
 from data_loaders.cvs_loader import CVSLoader
 from utils.label_convertors import convert2vec, hierarchical, convert2hier
 from utils.label_convertors import fill_unlabeled
@@ -31,11 +30,11 @@ def main(data_path,
 
     x_train = convert2vec(x_train, dtype=float)
     y_train = convert2hier(y_train, dtype=float)
-    
+
     x_test = convert2vec(x_test, dtype=float)
-    y_val = convert2hier(y_test, dtype=float) # for validation during training
-    y_eval = convert2vec(y_test, dtype=int)   # for evaluation after training
-    
+    y_val = convert2hier(y_test, dtype=float)  # for validation during training
+    y_eval = convert2vec(y_test, dtype=int)    # for evaluation after training
+
     data_pred = data_loader.load_unlabeled(["ECFP", "Label"])
     x_pred = data_pred[:, 0]
     x_pred = convert2vec(x_pred, dtype=float)
@@ -61,14 +60,14 @@ def main(data_path,
         log_fh=log_f,
         comment=comment
     )
-    
+
     # Train model1
-    ## Initialize model1
+    # - Initialize model1
     model1 = HMLC(drop_rate=drop_rate)
-    ## Training
+    # - Training
     my_train_model(model1, x_train, y_train, x_test, y_val, y_eval)
 
-    ## Predict labels for unlabeled data with model1
+    # - Predict labels for unlabeled data with model1
     predictions = model1.predict(x_pred)[:, -5:]
     y_pred_soft = fill_unlabeled(
         predictions, data_pred[:, 1], hard_label=False)
@@ -77,7 +76,7 @@ def main(data_path,
     y_pred_hard = fill_unlabeled(predictions, data_pred[:, 1], hard_label=True)
     y_pred_hard = hierarchical(y_pred_hard)
 
-    ## Combine labeled and soft-labeled unlabeled training data
+    # - Combine labeled and soft-labeled unlabeled training data
     x_mix = np.concatenate([x_train, x_pred], axis=0)
     y_mix = np.concatenate([y_train, y_pred_soft], axis=0)
     randomed_idx = np.random.permutation(x_mix.shape[0])
@@ -87,10 +86,10 @@ def main(data_path,
     # Train model2 with soft labels
     tf.keras.backend.clear_session()
     model2 = HMLC_M(drop_rate=drop_rate)
-    ## Training
+    # - Training
     my_train_model(model2, x_mix, y_mix, x_test, y_val, y_eval)
 
-    ## Combine labeled and hard-labeled unlabeled training data
+    # - Combine labeled and hard-labeled unlabeled training data
     x_mix = np.concatenate([x_train, x_pred], axis=0)
     y_mix = np.concatenate([y_train, y_pred_hard], axis=0)
     randomed_idx = np.random.permutation(x_mix.shape[0])
@@ -100,10 +99,10 @@ def main(data_path,
     # Train model3 with hard labels
     tf.keras.backend.clear_session()
     model3 = HMLC_M(drop_rate=drop_rate)
-    ## Training
+    # - Training
     my_train_model(model3, x_mix, y_mix, x_test, y_val, y_eval)
     log_f.close()
-    
+
 
 if __name__ == "__main__":
     parser = TrainingArgs()

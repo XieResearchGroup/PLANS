0a4e52e0b5322e5dc336db9a82eb10b6c2ef78f5
Yang Liu <thomasliuy@gmail.com>
2020-01-30 12:35:52 -0500

    linting

--- src/utils/label_convertors.py
+++ src/utils/label_convertors.py
@@ -11,9 +11,9 @@ def convert2vec(data, dtype=int):
 
 def _if_true_label(label):
     try:
-        return np.expand_dims((np.sum(label, axis=1)>0).astype(np.int32), 1)
+        return np.expand_dims((np.sum(label, axis=1) > 0).astype(np.int32), 1)
     except np.AxisError:
-        return np.expand_dims(((label>0).astype(np.int32)), 1)
+        return np.expand_dims(((label > 0).astype(np.int32)), 1)
 
 
 def hierarchical(true_label):

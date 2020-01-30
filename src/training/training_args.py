7ae5f9ccf30430693a4ec02928de4dde5cc0100c
Yang Liu <thomasliuy@gmail.com>
2020-01-30 11:50:27 -0500

    linting

--- src/training/training_args.py
+++ src/training/training_args.py
@@ -1,5 +1,6 @@
 import argparse
 
+
 class TrainingArgs(argparse.ArgumentParser):
 
     def __init__(self):
@@ -11,9 +12,9 @@ class TrainingArgs(argparse.ArgumentParser):
         self.add_argument("-r", "--if-hard", action="store_true")
         self.add_argument("--es-patience", type=int, default=3,
                           help="early_stop callback patience.")
-        self.add_argument("--log-path", type=str, default="../logs", 
+        self.add_argument("--log-path", type=str, default="../logs",
                           help="path to the log directory")
         self.add_argument("-m", "--comment", type=str, default=None,
                           help="comment for the training")
         self.add_argument("--unlabeled-weight", type=float, default=0.5,
-                          help="weight of unlabeled data in loss function")               
+                          help="weight of unlabeled data in loss function")

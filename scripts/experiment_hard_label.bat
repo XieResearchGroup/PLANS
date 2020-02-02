34405b01a127b09630d705b47551a2437c7569f7
Yang Liu <thomasliuy@gmail.com>
2020-02-02 11:45:47 -0500

    fix batch file

--- scripts/experiment_hard_label.bat
+++ scripts/experiment_hard_label.bat
@@ -1,1 +1,7 @@
-python -m training.experiment_hard_label -p ..\data\fromraw_cid_inchi_smiles_fp_labels.csv -e 1 -b 256 -l 0.001 --es-patience 10
+python -m training.experiment_hard_label ^
+-p ..\data\fromraw_cid_inchi_smiles_fp_labels.csv ^
+-e 100 ^
+-b 256 ^
+-l 0.00001 ^
+--es-patience 10 ^
+--log-path ..\logs\soft_hard_labeling

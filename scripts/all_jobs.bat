39bcfae400effbe595d1ce2b52de0ef4353c0fea
Yang Liu <thomasliuy@gmail.com>
2020-02-02 18:49:50 -0500

    make training processes consistant

--- scripts/all_jobs.bat
+++ scripts/all_jobs.bat
@@ -1,63 +1,64 @@
-REM Training S, M, L models with Noisy Student
+REM REM Training S, M, L models with Noisy Student
 FOR /L %%i IN (1, 1, 5) DO ^
 python -m src.training.train_hmlc ^
 -p .\data\fromraw_cid_inchi_smiles_fp_labels.csv ^
 -e 100 ^
 -b 256 ^
--l 0.00001 ^
+-l 0.0001 ^
 --es-patience 10 ^
---unlabeled-weight 0.5 ^
---log-path ..\logs\NoisyStudent\test_%%i
+--unlabeled-weight 1.0 ^
+--log-path .\logs\NoisyStudent\test_%%i
 
-REM Test M and L models with only the labeled data
+REM REM Test M and L models with only the labeled data
 FOR /L %%i IN (1, 1, 5) DO ^
 python -m src.training.experiment_M_L_wo_ns ^
--p ..\data\fromraw_cid_inchi_smiles_fp_labels.csv ^
+-p .\data\fromraw_cid_inchi_smiles_fp_labels.csv ^
 -e 100 ^
 -b 256 ^
--l 0.00001 ^
+-l 0.0001 ^
 --es-patience 10 ^
---log-path ..\logs\ML_w_labeled_only\test_%%i
+--log-path .\logs\ML_w_labeled_only\test_%%i ^
+--unlabeled-weight 1.0
 
-REM Test whether soft labeling is better than hard labeling
-FOR /L %%i IN (1, 1, 5) DO ^
-python -m src.training.experiment_hard_label ^
--p ..\data\fromraw_cid_inchi_smiles_fp_labels.csv ^
--e 100 ^
--b 256 ^
--l 0.00001 ^
---es-patience 10 ^
---log-path ..\logs\soft_hard_labeling\test_%%i
+REM REM Test whether soft labeling is better than hard labeling
+REM FOR /L %%i IN (1, 1, 5) DO ^
+REM python -m src.training.experiment_hard_label ^
+REM -p .\data\fromraw_cid_inchi_smiles_fp_labels.csv ^
+REM -e 100 ^
+REM -b 256 ^
+REM -l 0.0001 ^
+REM --es-patience 10 ^
+REM --log-path .\logs\soft_hard_labeling\test_%%i ^
+REM --unlabeled-weight 1.0
 
-REM Test adding loss weights
+REM REM Test adding loss weights
 FOR /L %%i IN (1, 1, 5) DO ^
 python -m src.training.experiment_exploit_unlabeled ^
 -p .\data\fromraw_cid_inchi_smiles_fp_labels.csv ^
 -e 100 ^
 -b 256 ^
--l 0.00001 ^
+-l 0.0001 ^
 --es-patience 10 ^
---unlabeled-weight 0.5 ^
---log-path ..\logs\loss_w_weights\test_%%i
+--log-path .\logs\loss_w_weights\test_%%i
 
-REM Test artificial labeling
+REM REM Test artificial labeling
 FOR /L %%i IN (1, 1, 5) DO ^
 python -m src.training.experiment_artificial_label ^
 -p .\data\fromraw_cid_inchi_smiles_fp_labels.csv ^
 -e 100 ^
 -b 256 ^
--l 0.00001 ^
+-l 0.0001 ^
 --es-patience 10 ^
---unlabeled-weight 0.5 ^
---log-path ..\logs\artificial_labeling\test_%%i
+--unlabeled-weight 1.0 ^
+--log-path .\logs\artificial_labeling\test_%%i
 
-REM Test Larger HMLC models with Noisy Student
+REM REM Test Larger HMLC models with Noisy Student
 FOR /L %%i IN (1, 1, 5) DO ^
 python -m src.training.experiment_larger_models ^
 -p .\data\fromraw_cid_inchi_smiles_fp_labels.csv ^
 -e 100 ^
 -b 256 ^
--l 0.00001 ^
+-l 0.0001 ^
 --es-patience 10 ^
 --unlabeled-weight 0.5 ^
 --log-path .\logs\lager_NoisyStudent\test_%%i

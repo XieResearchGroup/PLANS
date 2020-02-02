fa86e83abdedb20ca7eb7bd995682de9168fca6f
Yang Liu <thomasliuy@gmail.com>
2020-02-02 11:46:03 -0500

    optimize

--- scripts/experiment_M_L_wo_ns.bat
+++ scripts/experiment_M_L_wo_ns.bat
@@ -1,1 +1,5 @@
-python -m training.experiment_M_L_wo_ns -p ..\data\fromraw_cid_inchi_smiles_fp_labels.csv -e 100 -b 256 -l 0.001
+python -m training.experiment_M_L_wo_ns ^
+-p ..\data\fromraw_cid_inchi_smiles_fp_labels.csv ^
+-e 100 ^
+-b 256 ^
+-l 0.00001

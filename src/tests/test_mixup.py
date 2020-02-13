4533ed0ada0a8e18aae8e5dd404da35c401aa1af
Yang Liu <thomasliuy@gmail.com>
2020-02-13 11:57:25 -0500

    tests

---
+++ src/tests/test_mixup.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+from ..utils.mixup import mixup
+
+batch_size = 5
+a = np.round(np.random.rand(batch_size, 4))
+label_a = np.round(np.random.rand(batch_size, 5))
+b = np.round(np.random.rand(batch_size, 4))
+label_b = np.round(np.random.rand(batch_size, 5))
+
+sample, label = mixup(0.4, 0.4, a, label_a, b, label_b)
+print("a is: \n{}".format(a))
+print("b is: \n{}".format(b))
+print("mixed samples are: \n{}".format(sample))
+print()
+print("label of a is: \n{}".format(label_a))
+print("label of b is: \n{}".format(label_b))
+print("mixed labels are: \n{}".format(label))
+print()
+
+sample, label = mixup(0.4, 0.4, a, label_a)
+print("mix the samples in the same group by random permutation.")
+print("result: \n{}".format(sample))
+print("labels: \n{}".format(label))

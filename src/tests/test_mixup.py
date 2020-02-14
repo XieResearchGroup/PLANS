e72d33b54728d018a1b92010256e31d52bbb26ac
Yang Liu <thomasliuy@gmail.com>
2020-02-14 14:38:38 -0500

    move parameter to beginning of the script

--- src/tests/test_mixup.py
+++ src/tests/test_mixup.py
@@ -3,12 +3,16 @@ import numpy as np
 from ..utils.mixup import mixup
 
 batch_size = 5
+rep = 2
+shuffle = True
+
 a = np.round(np.random.rand(batch_size, 4))
 label_a = np.round(np.random.rand(batch_size, 5))
 b = np.round(np.random.rand(batch_size, 4))
 label_b = np.round(np.random.rand(batch_size, 5))
 
-sample, label = mixup(0.4, 0.4, a, label_a, b, label_b)
+sample, label = mixup(0.4, 0.4, a, label_a,
+                      sample_2=b, label_2=label_b, repeat=rep, shuffle=shuffle)
 print("a is: \n{}".format(a))
 print("b is: \n{}".format(b))
 print("mixed samples are: \n{}".format(sample))
@@ -18,7 +22,7 @@ print("label of b is: \n{}".format(label_b))
 print("mixed labels are: \n{}".format(label))
 print()
 
-sample, label = mixup(0.4, 0.4, a, label_a)
+sample, label = mixup(0.4, 0.4, a, label_a, repeat=rep, shuffle=shuffle)
 print("mix the samples in the same group by random permutation.")
 print("result: \n{}".format(sample))
 print("labels: \n{}".format(label))

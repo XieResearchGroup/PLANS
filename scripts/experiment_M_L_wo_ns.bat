d85ba02cf6373452ee4059035a28bd51df551dcc
Yang Liu <thomasliuy@gmail.com>
2020-02-16 20:50:09 -0500

    add loop

--- scripts/experiment_M_L_wo_ns.bat
+++ scripts/experiment_M_L_wo_ns.bat
@@ -1,5 +1,9 @@
-python -m training.experiment_M_L_wo_ns ^
--p ..\data\fromraw_cid_inchi_smiles_fp_labels.csv ^
--e 100 ^
+@echo off
+FOR /L %%i IN (1, 1, 5) DO ^
+python -m src.training.experiment_M_L_wo_ns ^
+-p .\data\fromraw_cid_inchi_smiles_fp_labels.csv ^
+-e 1000 ^
 -b 256 ^
--l 0.00001
+-l 0.00005 ^
+--es-patience 10 ^
+--log-path .\logs\ML_w_labeled_only
--- scripts/train_hmlc.bat
+++ scripts/train_hmlc.bat
@@ -1,8 +1,10 @@
+@echo off
+FOR /L %%i IN (1, 1, 5) DO ^
 python -m src.training.train_hmlc ^
 -p .\data\fromraw_cid_inchi_smiles_fp_labels.csv ^
--e 1 ^
+-e 1000 ^
 -b 256 ^
--l 0.001 ^
---es-patience 5 ^
---unlabeled-weight 0.5
+-l 0.00001 ^
+--es-patience 10 ^
+--unlabeled-weight 1.0 ^
 --log-path .\logs\hmlc

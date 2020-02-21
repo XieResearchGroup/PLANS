c13567f04c933a4f0b24430a924ee891a38235d3
Yang Liu <thomasliuy@gmail.com>
2020-02-21 11:55:06 -0500

    add outside data path

--- src/training/training_args.py
+++ src/training/training_args.py
@@ -5,7 +5,8 @@ class BaseArgs(argparse.ArgumentParser):
 
     def __init__(self):
         super(BaseArgs, self).__init__()
-        self.add_argument("-p", "--data-path")
+        self.add_argument("-p", "--data-path",
+                          help="Path to the training dataset.")
         self.add_argument("-b", "--batch-size", type=int, default=32)
         self.add_argument("-e", "--epochs", type=int, default=100)
         self.add_argument("--es-patience", type=int, default=3,
@@ -19,7 +20,9 @@ class TrainingArgs(BaseArgs):
     def __init__(self):
         super(TrainingArgs, self).__init__()
         self.add_argument("-l", "--learning-rate", type=float, default=0.001)
-        self.add_argument("-r", "--if-hard", action="store_true")
+        self.add_argument("-r", "--if-hard", action="store_true",
+                          help="If to use hard label when applying Noisy "
+                          "Student method")
         self.add_argument("-m", "--comment", type=str, default=None,
                           help="comment for the training")
         self.add_argument("--unlabeled-weight", type=float, default=0.5,
@@ -33,3 +36,11 @@ class LinearModelTrainingArgs(BaseArgs):
         self.add_argument("--repeat", type=int, default=3,
                           help="Times to repeat when training the model with "
                           "Noisy Student.")
+
+
+class LMOutsideDataArgs(LinearModelTrainingArgs):
+
+    def __init__(self):
+        super(LMOutsideDataArgs, self).__init__()
+        self.add_argument("--outside-path",
+                          help="Path to the outside dataset.")

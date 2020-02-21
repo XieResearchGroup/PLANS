1b320bea4542274fc3e1d5d7c33c01d6fb18b83c
Yang Liu <thomasliuy@gmail.com>
2020-02-20 19:39:28 -0500

    reduce batch size

--- scripts/experiment_linear_models.bat
+++ scripts/experiment_linear_models.bat
@@ -1,7 +1,7 @@
 python -m src.training.experiment_linear_models ^
 -p .\data\fromraw_cid_inchi_smiles_fp_labels_onehots.csv ^
 -e 200 ^
--b 256 ^
+-b 128 ^
 --es-patience 10 ^
 --log-path .\logs\linear ^
 --repeat 3

e73994801a5b153c08f29a884987b9fbb46f75a7
Yang Liu <thomasliuy@gmail.com>
2020-02-27 16:59:59 -0500

    linting

--- src/data_loaders/_base_loader.py
+++ src/data_loaders/_base_loader.py
@@ -1,11 +1,12 @@
 # Base class of the dataloaders
 import abc
 
+
 class _BaseDataLoader:
 
     def __init__(self, path):
         self.path = path
-    
+
     @abc.abstractmethod
     def load_data(self):
         raise NotImplementedError

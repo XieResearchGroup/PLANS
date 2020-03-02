5c495f9dc49fe0115473b3399359e9616e7e2740
Yang Liu <thomasliuy@gmail.com>
2020-03-02 01:09:57 -0500

    training sripts

---
+++ scripts/experiment_linear_balance_classes.sh
@@ -0,0 +1,9 @@
+CUDA_VISIBLE_DEVICES=$1 \
+python -m src.training.experiment_linear_balance_classes \
+-p ./data/fromraw_cid_inchi_smiles_fp_labels_onehots.csv \
+--outside-path ./data/DrugBank_smiles_fp.csv \
+-e 1000 \
+-b 256 \
+--es-patience 20 \
+--log-path ./logs/linear/drugbank_mixup \
+--repeat 5

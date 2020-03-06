8c9c7ebe89e7e1157c92a61528c07ad273f02ed3
Yang Liu <thomasliuy@gmail.com>
2020-03-06 16:23:24 -0500

    add max-depth and rand-seed arguments

--- scripts/experiment_conventional_multiclass.bat
+++ scripts/experiment_conventional_multiclass.bat
@@ -1,4 +1,6 @@
 python -m src.training.experiment_conventional_multiclass ^
 -p .\data\fromraw_cid_inchi_smiles_fp_labels_onehots.csv ^
 --log-path .\logs\convention ^
---n-estimators 500
+--max-depth 5 ^
+--n-estimators 500 ^
+--rand-seed 0

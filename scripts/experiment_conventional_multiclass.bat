8235f7ad4821d206acca5f66107d4bbe3288888d
Yang Liu <thomasliuy@gmail.com>
2020-03-07 23:13:34 -0500

    script for conventional models experiments

--- scripts/experiment_conventional_multiclass.bat
+++ scripts/experiment_conventional_multiclass.bat
@@ -1,6 +1,6 @@
-python -m src.training.experiment_conventional_multiclass ^
+FOR /L %%i IN (1, 1, 5) DO python -m src.training.experiment_conventional_multiclass ^
 -p .\data\fromraw_cid_inchi_smiles_fp_labels_onehots.csv ^
---log-path .\logs\convention ^
---max-depth 5 ^
+--log-path .\logs\convention\test_1 ^
+--max-depth 14 ^
 --n-estimators 500 ^
---rand-seed 0
+--rand-seed %%i
